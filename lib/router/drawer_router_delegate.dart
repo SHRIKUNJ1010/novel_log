39af7c69d6dea45d56cdc65c35ac14882eb28809
Shrikunj Patel <spatel1@sheresolved.com>
2023-02-08 17:31:34 +0530

    transition delegate removed and drawer transition works normally

--- lib/router/drawer_router_delegate.dart
+++ lib/router/drawer_router_delegate.dart
@@ -12,7 +12,6 @@ import 'package:novel_log/screens/app_drawer_screens/novel_hidden_list_screen.da
 import 'package:novel_log/screens/app_drawer_screens/novel_wish_list_screen.dart';
 import 'package:novel_log/screens/app_drawer_screens/profile_screen.dart';
 import 'package:novel_log/screens/app_drawer_screens/your_novel_list_screen.dart';
-import 'package:novel_log/utility/drawer_transition_delegate.dart';
 import 'package:novel_log/utility/enum_variable_types.dart';
 import 'package:novel_log/utility/page_config_list.dart';
 import 'package:novel_log/utility/page_routes.dart';
@@ -36,7 +35,6 @@ class DrawerRouterDelegate extends RouterDelegate<List<PageConfiguration>> with 
       key: navigatorKey,
       pages: buildPages(),
       onPopPage: _onPopPage,
-      transitionDelegate: const DrawerTransitionDelegate(),
     );
   }
 
